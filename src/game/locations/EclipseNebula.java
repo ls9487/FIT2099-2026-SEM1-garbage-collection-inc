43209251637c77b16930f4b12ab850bee489f52e
Your Name <esoo0013@student.monash.edu>
2026-05-08 00:30:32 +0800

    REQ1: EclipseNebula.java:       - Added a Tester Helper Comment in and out to either have the player start broke (// kept) OR have the player start with max amount of money (1000) (// removed) for easy test casing

--- src/game/locations/EclipseNebula.java
+++ src/game/locations/EclipseNebula.java
@@ -56,6 +56,17 @@ public class EclipseNebula extends World {
         //this.addPlayer(contractedWorker3, moon99DeprecatedMap.at(8, 2));
         //this.addPlayer(contractedWorker4, moon99DeprecatedMap.at(6, 4));
         //this.addPlayer(contractedWorker5, moon99DeprecatedMap.at(8, 4));
+
+        /*
+         * TESTING HELPER for any REQ involving credit amount:
+         * Uncomment this line when testing REQ involving purchases
+         * (FirstAidKit, SterilisationBox, AccessCards, Creatures, etc.)
+         * since workers normally start with 0 credits.
+         *
+         * Comment it out again for normal gameplay/evaluation (start as a broke).
+         */
+        //contractedWorker1.getWallet().add(1000);
+
     }
 
     /**
