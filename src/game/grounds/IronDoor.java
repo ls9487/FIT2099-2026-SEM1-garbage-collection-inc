fccb4e3aa68dc9ab3c3cc3ee8101194d0c0be329
Edmond <eche0116@student.monash.edu>
2026-05-12 23:35:31 +0800

    Fully developed IronDoor class
    - requiredClearance method that checks the clearance level for AccessCard 2
    
    - unlockSideEffects that finds the door location then sets the door's adjacent tiles on fire

---
+++ src/game/grounds/IronDoor.java
@@ -0,0 +1,72 @@
+package game.grounds;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.items.ClearanceLevel;
+import game.items.Fire;
+
+import java.util.List;
+
+/**
+ * Tier-2 door that overheats neighbouring floor tiles when opened.
+ *
+ * @author eche0116
+ * @version 1.0
+ */
+public class IronDoor extends Door {
+
+    private static final int FIRE_DURATION = 5;
+    private static final int NEARBY_RADIUS = 1;
+
+    /**
+     * Creates an iron door marked with {@code N} on facility plans.
+     *
+     * @author eche0116
+     * @version 1.0
+     */
+    public IronDoor() {
+        super('N', "Iron Door");
+    }
+
+    /**
+     * Declares the minimum clearance tier required to unlock this door.
+     *
+     * @return clearance level two
+     * @author eche0116
+     * @version 1.0
+     */
+    @Override
+    protected ClearanceLevel requiredClearance() {
+        return ClearanceLevel.LEVEL_2;
+    }
+
+    /**
+     * Unlock IronDoor will ignite adjacent floor tiles
+     *
+     * @param actor the unlocking worker
+     * @param map   the map used to locate adjacent floors
+     * @return unlock narrative including fire spread
+     * @author eche0116
+     * @version 1.0
+     */
+    @Override
+    protected String unlockSideEffects(Actor actor, GameMap map) {
+        Location doorLocation = null;
+        for (Location location : map.locationOf(actor).getNearbyLocations(NEARBY_RADIUS)) {
+            if (location.getGround() == this) {
+                doorLocation = location;
+                break;
+            }
+        }
+        if (doorLocation != null) {
+            for (Location neighbour : doorLocation.getNearbyLocations(NEARBY_RADIUS)) {
+                if (neighbour.getGround().canActorEnter(actor)) {
+                    neighbour.addItem(new Fire(FIRE_DURATION));
+                }
+            }
+        }
+        return actor + " unlocked the iron door. The mechanism overheats, igniting adjacent floor tiles.";
+    }
+}
