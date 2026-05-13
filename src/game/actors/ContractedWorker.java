e47158073b2bc8cdd4576db096a6cb83270847c1
Edwin Chuah Wei-Hann <echu0057@student.monash.edu>
2026-05-13 16:56:09 +0800

    Added immediate spawn effects to Slime and Parasite
    -Slime now causes adjacent susceptible actors to drop all their carried items
    -Parasite now deals 2 damage to adjacent susceptible actors
    -Added abilities to ActorAbilities to denote actors susceptible to those effects
    -ContractedWorker has been given those susceptible abilities

--- src/game/actors/ContractedWorker.java
+++ src/game/actors/ContractedWorker.java
@@ -33,6 +33,8 @@ public class ContractedWorker extends EclipseActor implements Infectable, Spawne
     public ContractedWorker(String name, char displayChar, int hitPoints, Inventory inventory) {
         super(name, displayChar, hitPoints, inventory);
         this.enableAbility(ActorAbilities.VENT_ACTIVATOR);
+        this.enableAbility(ActorAbilities.SLIME_EFFECT_SUSCEPTIBLE);
+        this.enableAbility(ActorAbilities.PARASITE_EFFECT_SUSCEPTIBLE);
     }
 
     /**
--- src/game/actors/Parasite.java
+++ src/game/actors/Parasite.java
@@ -7,7 +7,9 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actors.ActorStatistics;
 import edu.monash.fit2099.engine.behaviours.Behaviour;
 import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
 import game.behaviours.InfectBehaviour;
 import game.behaviours.WanderBehaviour;
 import game.inventories.BasicInventory;
@@ -19,7 +21,7 @@ import game.inventories.BasicInventory;
  *
  * @author echu0057
  */
-public class Parasite extends EclipseActor implements Infector {
+public class Parasite extends EclipseActor implements Infector, EnvironmentalTriggerer {
     private static final int INFECT_BEHAVIOUR_PRIORITY = 1;
     private static final int WANDER_BEHAVIOUR_PRIORITY = 999;
 
@@ -80,6 +82,24 @@ public class Parasite extends EclipseActor implements Infector {
         // Do damage equal to the parasite's max hp (instantly killing it).
         this.hurt(this.getMaximumStatistic(ActorStatistics.HEALTH));
         return String.format("%s dies as a result.", this);
+    }
+
+    /**
+     * Upon being spawned, deal 2 damage to actors susceptible to this effect (e.g. workers).
+     * @param location The location where the parasite was spawned.
+     */
+    @Override
+    public void onSpawnEffect(Location location) {
+        final int ADJACENT_DAMAGE = 2;
+        // Check each adjacent location.
+        for (Exit exit : location.getExits()) {
+            Location destination = exit.getDestination();
+            // Check if there's a susceptible actor. If so, deal damage to it.
+            if (destination.containsAnActor() &&
+                    destination.getActor().hasAbility(ActorAbilities.PARASITE_EFFECT_SUSCEPTIBLE)) {
+                destination.getActor().hurt(ADJACENT_DAMAGE);
+            }
+        }
     }
 
 }
