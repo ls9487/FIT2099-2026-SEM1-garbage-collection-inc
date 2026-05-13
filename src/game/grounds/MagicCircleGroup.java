90a00dd55f73ba4ad482b9b3f6206024d349cb46
Edmond <eche0116@student.monash.edu>
2026-05-13 17:45:31 +0800

    Fully developed MagicCircleGroup added:
    - constructor that creates sites as an array list
    
    - register() add a new site
    
    - randomPeerExcluding() method that chooses a different circle on the same map as the source, if possible

---
+++ src/game/grounds/MagicCircleGroup.java
@@ -0,0 +1,52 @@
+package game.grounds;
+
+import edu.monash.fit2099.engine.positions.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Shared registry of linked magic circle sites used for random within-map jumps.
+ *
+ * @author eche0116
+ * @version 1.0
+ */
+public final class MagicCircleGroup {
+
+    private static final Random random = new Random();
+
+    private final List<Location> sites;
+
+    public MagicCircleGroup() {
+        sites = new ArrayList<>();
+    }
+    /**
+     * Registers a circle location after it has been placed on a map.
+     *
+     * @param site circle tile
+     */
+    public void register(Location site) {
+        sites.add(site);
+    }
+
+    /**
+     * Chooses a different circle on the same map as the source, if possible.
+     *
+     * @param from the circle currently being used
+     * @return a destination circle tile, or null when no peer exists
+     */
+    public Location randomPeerExcluding(Location from) {
+        List<Location> peers = new ArrayList<>();
+        for (Location candidate : sites) {
+            if (candidate != from && candidate.map() == from.map() && !candidate.containsAnActor()) {
+                peers.add(candidate);
+            }
+        }
+        if (peers.isEmpty()) {
+            return null;
+        }
+
+        return peers.get(random.nextInt(peers.size()));
+    }
+}
