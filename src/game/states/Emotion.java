0f63b3bcb76f58968cbe6627f086703f7c4f1543
lyan0121 <lyan0121@student.monash.edu>
2026-05-15 21:12:46 +0800

    Added javadocs to DragItemOperations, Emotion, State and ValuableItemOperations

--- src/game/states/Emotion.java
+++ src/game/states/Emotion.java
@@ -1,5 +1,11 @@
 package game.states;
 
+/**
+ * Discrete moods that drive State selection for StatefulCreature.
+ *
+ * @author lyan0121
+ * @version 1.0
+ */
 public enum Emotion {
     CAUTION,
     MISCHIEVOUS,
