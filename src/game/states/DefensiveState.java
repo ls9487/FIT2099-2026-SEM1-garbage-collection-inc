a25370c9b0f91c37aad6f19d2670ad179b092a0a
lyan0121 <lyan0121@student.monash.edu>
2026-05-15 12:26:43 +0800

    Implement DefensiveState - Implement DefensiveState which will pull item on ground within its reachable range when it enters the State and overrides AttackBehaviour to the StatefulCreature

---
+++ src/game/states/DefensiveState.java
@@ -0,0 +1,43 @@
+package game.states;
+
+import edu.monash.fit2099.engine.actors.Actor;
+
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.StatefulCreature;
+import game.behaviours.AttackBehaviour;
+
+public class DefensiveState extends State {
+
+
+    public DefensiveState(StatefulCreature statefulCreature) {
+        super(statefulCreature);
+        addNewBehaviour(1, new AttackBehaviour());
+
+    }
+
+    @Override
+    public Emotion transition(Actor actor, GameMap map) {
+
+
+        if (this.workerDetection(getActorVigilanceRange(), map.locationOf(actor))) {
+            if (!isActorInventoryFull())
+                return Emotion.FEARFUL;
+        } else {
+            return Emotion.GREEDY;
+        }
+        return Emotion.ANGRY;
+
+
+    }
+
+    @Override
+    public void immediateEffect(Location location) {
+        // pull vigilanceRange item by 1
+        Display display = new Display();
+        dragItemOnGround(getActorVigilanceRange(), location, DragItemOperations.PULL);
+        display.println(String.format("%s pulls items on ground within %d radius by 1", getStatefulCreature(), getActorVigilanceRange()));
+
+    }
+}
