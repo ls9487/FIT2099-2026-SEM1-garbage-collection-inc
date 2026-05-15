6fb94dec561b004d1c58df619a4f5edc791df7b6
lyan0121 <lyan0121@student.monash.edu>
2026-05-15 11:56:40 +0800

    Implement HoardingState - Implement HoardingState which will drops the least valuable item from inventory when it enters the State and overrides WanderBehaviour to the StatefulCreature

---
+++ src/game/states/HoardingState.java
@@ -0,0 +1,38 @@
+package game.states;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.DropAction;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.StatefulCreature;
+import game.behaviours.WanderBehaviour;
+
+public class HoardingState extends State {
+    private static final int WANDER_BEHAVIOUR_PRIORITY = 1;
+
+    public HoardingState(StatefulCreature statefulCreature) {
+        super(statefulCreature);
+        addNewBehaviour(WANDER_BEHAVIOUR_PRIORITY, new WanderBehaviour());
+
+    }
+
+
+    @Override
+    public Emotion transition(Actor actor, GameMap map) {
+        if (this.workerDetection(getActorVigilanceRange(), map.locationOf(actor))) {
+            return Emotion.ANGRY;
+        } else {
+            if (!isActorInventoryFull())
+                return Emotion.CURIOUS;
+        }
+        return Emotion.GREEDY;
+    }
+
+    @Override
+    public void immediateEffect(Location location) {
+        // drops the least valuable item from inventory
+        Item item = getValuableItem(getStatefulCreature(), ValuableItemOperations.LEAST);
+        new DropAction(item).execute(getStatefulCreature(), location.map());
+    }
+}
