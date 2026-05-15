0cec854aea10c548ff88211fe810f8e67dd0635a
lyan0121 <lyan0121@student.monash.edu>
2026-05-15 11:41:47 +0800

    Implement StunStatus and Stunnable - Implement StunStatus for actor to be stunned, when actor are stunned, their turns are skipped and hurts every turn

---
+++ src/game/statuses/StunStatus.java
@@ -0,0 +1,48 @@
+package game.statuses;
+
+import edu.monash.fit2099.engine.GameEntity;
+import edu.monash.fit2099.engine.capabilities.Status;
+import edu.monash.fit2099.engine.positions.Location;
+
+/**
+ * StunStatus represents a status effect of being stunned.
+ * when being stunned actor's turn are skipped
+ * @author lyan0121
+ * @version 1.0
+ */
+public class StunStatus implements Status {
+    private int duration;
+    private int intensity;
+    private Stunnable stunnable;
+
+    /**
+     * Constructor for the StunStatus class.
+     * @param duration The number of ticks the stun lasts.
+     */
+    public StunStatus(int duration, int intensity, Stunnable stunnable) {
+        this.duration = duration;
+        this.intensity = intensity;
+        this.stunnable = stunnable;
+    }
+    /**
+     * Called once per tick to update the status of the current entity.
+     * Whatever the burn does is up to the entity, taking damage for example.
+     * @param currEntity The entity this status is attached to.
+     */
+    @Override
+    public void tickStatus(GameEntity currEntity, Location location) {
+        if (stunnable != null) {
+            duration--;
+            stunnable.stun(intensity);
+        }
+    }
+    /**
+     * Indicates whether this status is still active.
+     *
+     * @return true if active, false otherwise
+     */
+    @Override
+    public boolean isStatusActive() {
+        return duration > 0;
+    }
+}
