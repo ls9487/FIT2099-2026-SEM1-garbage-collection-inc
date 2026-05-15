93ae1e2b90fac1f7e27b5e918c5720dcfe089515
lyan0121 <lyan0121@student.monash.edu>
2026-05-15 11:52:01 +0800

    Implement ScavengeState - Implement ScavengeState which will steal the nearest worker most valuable items when it enters the State and overrides PickItemBehaviour and FindItemBehaviour to the StatefulCreature

---
+++ src/game/states/ScavengeState.java
@@ -0,0 +1,41 @@
+package game.states;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.StatefulCreature;
+import game.behaviours.FindItemBehaviour;
+import game.behaviours.PickItemBehaviour;
+
+public class ScavengeState extends State {
+    private static final int PICK_ITEM_BEHAVIOUR_PRIORITY = 1;
+    private static final int FIND_ITEM_BEHAVIOUR_PRIORITY = 2;
+    public ScavengeState(StatefulCreature statefulCreature) {
+        super(statefulCreature);
+        addNewBehaviour(PICK_ITEM_BEHAVIOUR_PRIORITY, new PickItemBehaviour());
+        addNewBehaviour(FIND_ITEM_BEHAVIOUR_PRIORITY, new FindItemBehaviour(getActorVigilanceRange()));
+    }
+
+    @Override
+    public Emotion transition(Actor actor, GameMap map) {
+        if (this.workerDetection(getActorVigilanceRange(), map.locationOf(actor))) {
+            return Emotion.FEARFUL;
+        } else {
+            if (isActorInventoryFull()) {
+                return Emotion.GREEDY;
+            }
+        }
+        return Emotion.CURIOUS;
+    }
+
+
+    @Override
+    public void immediateEffect(Location location) {
+        // steal nearest player most valuable item
+        Actor nearestPlayer = nearestWorker(getActorVigilanceRange(), location);
+        Item item = getValuableItem(nearestPlayer, ValuableItemOperations.MOST);
+        nearestPlayer.getInventory().remove(item);
+        getStatefulCreature().getInventory().add(item);
+    }
+}
