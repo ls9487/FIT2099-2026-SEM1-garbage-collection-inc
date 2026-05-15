9db476645d47bfcf229997efa86a6e13661032a2
lyan0121 <lyan0121@student.monash.edu>
2026-05-15 20:39:54 +0800

    Fix TeleportationTube teleport location wrong problem

--- src/game/locations/EclipseNebula.java
+++ src/game/locations/EclipseNebula.java
@@ -68,7 +68,7 @@ public class EclipseNebula extends World {
         for (Location tubeLocation : moon99DeprecatedMap.getTubeLocations()) {
             List<Location> tubeTeleportableLocation = new ArrayList<>();
             tubeTeleportableLocation.add(moon99DeprecatedMap.at(6, 3));
-            tubeTeleportableLocation.add(overflow20Map.at(3, 3));
+            tubeTeleportableLocation.add(overflow20Map.at(6, 3));
             tubeLocation.setGround(new TeleportationTube(tubeTeleportableLocation));
         }
 
@@ -76,6 +76,7 @@ public class EclipseNebula extends World {
             List<Location> tubeTeleportableLocation = new ArrayList<>();
             tubeTeleportableLocation.add(overflow20Map.at(10, 15));
             tubeTeleportableLocation.add(overflow20Map.at(30, 4));
+            tubeTeleportableLocation.add(overflow20Map.at(6, 3));
             tubeTeleportableLocation.add(moon99DeprecatedMap.at(6, 3));
             tubeLocation.setGround(new TeleportationTube(tubeTeleportableLocation));
         }
