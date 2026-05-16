4e3751848e70759e601c6638cb7eab57ce360340
lyan0121 <lyan0121@student.monash.edu>
2026-05-16 10:37:11 +0800

    Refactor inventory full detection for Muckraker and PhantasmWisp - Remove numberOfItemHolding attribute and use get inventory size to check if inventory is full

--- src/game/actors/Muckraker.java
+++ src/game/actors/Muckraker.java
@@ -6,6 +6,12 @@ import game.states.*;
 
 import java.util.TreeMap;
 
+/**
+ * Scavenging creature that hoards items and panics when workers are nearby.
+ *
+ * @author lyan0121
+ * @version 1.0
+ */
 public class Muckraker extends StatefulCreature {
     private static final String NAME = "Muckraker";
     private static final char DISPLAY_CHAR = 'Д';
@@ -14,27 +20,39 @@ public class Muckraker extends StatefulCreature {
     private static final int INVENTORY_SIZE = 3;
     private static final int VIGILANCE_RANGE = 3;
 
-    private int numberOfItemHolding;
+
+    /**
+     * Creates a Muckraker with default stats and emotion states.
+     * */
     public Muckraker() {
         super(NAME, DISPLAY_CHAR, HIT_POINTS, initialiseInventory(), INITIAL_EMOTION, new TreeMap<>());
-        numberOfItemHolding = 0;
         addNewState(Emotion.CURIOUS, new ScavengeState(this));
         addNewState(Emotion.GREEDY, new HoardingState(this));
         addNewState(Emotion.FEARFUL, new PanicState(this));
         addNewState(Emotion.ANGRY, new DefensiveState(this));
     }
 
+    /**
+     * initialise Muckraker inventory
+     * @return the inventory of Muckraker which is an ItemLimitedInventory
+     */
     private static Inventory initialiseInventory() {
         return new ItemLimitedInventory(INVENTORY_SIZE);
     }
 
+    /**
+     * @return how far this creature senses workers
+     */
     @Override
     public int getVigilanceRange() {
         return VIGILANCE_RANGE;
     }
 
+    /**
+     * @return true when the creature cannot pick up more items
+     */
     @Override
     public boolean isInventoryFull() {
-        return numberOfItemHolding >= INVENTORY_SIZE;
+        return this.getInventory().getItems().size() == INVENTORY_SIZE;
     }
 }
