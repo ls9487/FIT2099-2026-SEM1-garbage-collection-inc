91f557063a384f1da1e987789d56d55a1e75405f
lyan0121 <lyan0121@student.monash.edu>
2026-05-16 10:35:29 +0800

    Add javadocs - Add javadocs for DefensiveState, FleeState, HoardingState, LureState, PanicState, ScavengeState, StatefulCreature

--- src/game/states/FleeState.java
+++ src/game/states/FleeState.java
@@ -8,29 +8,56 @@ import edu.monash.fit2099.engine.positions.Location;
 import game.actors.StatefulCreature;
 import game.behaviours.WanderBehaviour;
 
-
+/**
+ * wanders and forces nearby workers to drop their most valuable item.
+ *
+ * @author lyan0121
+ * @version 1.0
+ */
 public class FleeState extends State {
     private static final int WANDER_BEHAVIOUR_PRIORITY = 1;
-    private static final int SURROUNDING_RADIUS = 1;
+    private static final int SURROUNDING = 1;
     private static final int ONE_WORKER = 1;
+
+    /**
+     * constructor
+     * @param statefulCreature owning creature
+     */
     public FleeState(StatefulCreature statefulCreature) {
         super(statefulCreature);
         addNewBehaviour(WANDER_BEHAVIOUR_PRIORITY, new WanderBehaviour());
     }
 
+    /**
+     * Chooses the emotion to enter after this turn's logic.
+     *
+     * if surrounding have exact 1 worker it will return curious emotion
+     * if vigilance range have exact 1 worker it will return vaution emotion
+     * if vigilance range have no worker it will return mischievous emotion
+     * else return fearful emotion
+     * @param actor the creature in this state
+     * @param map   the map the creature occupies
+     * @return the emotion for the next turn
+     */
     @Override
     public Emotion transition(Actor actor, GameMap map) {
-        if (workerNumber(SURROUNDING_RADIUS, map.locationOf(actor)) == ONE_WORKER) {
+        if (workerNumber(SURROUNDING, map.locationOf(actor)) == ONE_WORKER) {
             return Emotion.CURIOUS;
-        } else if (workerNumber(getActorVigilanceRange(), map.locationOf(actor)) == 1) {
+        } else if (workerNumber(getActorVigilanceRange(), map.locationOf(actor)) == ONE_WORKER) {
             return Emotion.CAUTION;
-        } else if (workerNumber(getActorVigilanceRange(), map.locationOf(actor)) == 0) {
+        } else if (!workerDetection(getActorVigilanceRange(), map.locationOf(actor))) {
             return Emotion.MISCHIEVOUS;
         } else {
             return Emotion.FEARFUL;
         }
     }
 
+    /**
+     * Applies a one-shot effect when entering this state.
+     *
+     * all player within vigilance range forced to drop most valuable item
+     * @param location tile occupied by the creature at transition time
+     */
     @Override
     public void immediateEffect(Location location) {
         Display display = new Display();
--- src/game/states/PanicState.java
+++ src/game/states/PanicState.java
@@ -9,18 +9,38 @@ import game.behaviours.FleeBehaviour;
 import game.statuses.StunStatus;
 import game.statuses.Stunnable;
 
+/**
+ * flees from workers and stuns actors in range on entry.
+ *
+ * @author lyan0121
+ * @version 1.0
+ */
 public class PanicState extends State {
 
     private static final int FLEE_BEHAVIOUR_PRIORITY = 1;
     private static final int STUN_DURATION = 2;
     private static final int STUN_DAMAGE = 1;
 
+    /**
+     * constructor
+     * @param statefulCreature owning creature
+     */
     public PanicState(StatefulCreature statefulCreature) {
         super(statefulCreature);
         addNewBehaviour(FLEE_BEHAVIOUR_PRIORITY, new FleeBehaviour(getActorVigilanceRange()));
 
     }
 
+    /**
+     * Chooses the emotion to enter after this turn's logic.
+     *
+     * if there exist worker in vigilance range it and its inventory is full it will return angry emotion
+     * if there doesn't exist worker in vigilance range it will return curious emotion
+     * else it will return fearful emotion
+     * @param actor the creature in this state
+     * @param map   the map the creature occupies
+     * @return the emotion for the next turn
+     */
     @Override
     public Emotion transition(Actor actor, GameMap map) {
         if (this.workerDetection(getActorVigilanceRange(), map.locationOf(actor))) {
@@ -32,6 +52,12 @@ public class PanicState extends State {
         return Emotion.FEARFUL;
     }
 
+    /**
+     * Applies a one-shot effect when entering this state.
+     *
+     * all worker within vigilance range will be stunned for 2 turns
+     * @param location tile occupied by the creature at transition time
+     */
     @Override
     public void immediateEffect(Location location) {
         Display display = new Display();
