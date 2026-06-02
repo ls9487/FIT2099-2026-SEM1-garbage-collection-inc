ff26ac6c71f7336d2760e6b5ed0d88a1defbf3cb
lyan0121 <lyan0121@student.monash.edu>
2026-06-02 16:29:51 +0800

    Implement Rideable abstract class - Implement Rideable extends EclipseItem, Rideable is not portable, - When there are actor stands above the rideable they can mount the rideable, and RideStatus will be given to the rider and rideable will be added into rider's inventory - When actor is riding on rideable they can dismount anytime, and RideStatus will be removed then the rider dismount from rideable and rideable will be removed from rider's inventory and placed on the ground below actor

---
+++ src/game/vehicles/Rideable.java
@@ -0,0 +1,89 @@
+package game.vehicles;
+
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actions.DismountAction;
+import game.actions.MountAction;
+import game.items.EclipseItem;
+import game.statuses.RideStatus;
+
+/**
+ * Abstract non-portable vehicle that an actor can mount or dismount.
+ * Mounting adds RideStatus to the rider and places this rideable in the rider's inventory
+ * dismounting removes the status and returns the rideable to the ground.
+ *
+ * @author lyan0121
+ * @version 1.0
+ */
+public abstract class Rideable extends EclipseItem {
+    /**
+     * Creates a rideable with the given display name and map character.
+     *
+     * @param name the name of this item
+     * @param displayChar the character used when this item is on the ground
+     */
+    public Rideable(String name, char displayChar) {
+        super(name, displayChar, 0);
+        makeNonPortable();
+    }
+
+    /**
+     * Mounts this rideable: removes it from the ground, adds it to the actor's inventory,
+     * and applies RideStatus to the rider.
+     *
+     * @param actor the actor mounting this rideable
+     * @param itemLocation the ground location where this rideable currently lies
+     * @return a short narrative description of the mount
+     */
+    public String mount(Actor actor, Location itemLocation) {
+        // cannot use pickup action then execute because non-portable
+        itemLocation.removeItem(this);
+        actor.getInventory().add(this);
+        actor.addStatus(new RideStatus());
+        return String.format("%s mounts %s", actor, this);
+    }
+
+    /**
+     * Dismounts this rideable: removes it from the actor's inventory, places it on the
+     * actor's current tile, and clears RideStatus from the rider.
+     *
+     * @param actor the actor dismounting this rideable
+     * @param map the map containing the actor
+     * @return a short narrative description of the dismount
+     */
+    public String dismount(Actor actor, GameMap map) {
+        actor.getInventory().remove(this);
+        map.locationOf(actor).addItem(this);
+        actor.removeStatus(new RideStatus());
+        return String.format("%s dismounts %s", actor, this);
+    }
+
+    /**
+     * List of allowable actions that can be performed on the item when it is on the ground
+     *
+     * @param location the location of the ground on which the item lies
+     * @return an unmodifiable list of Actions
+     */
+    @Override
+    public ActionList allowableActions(Location location) {
+        ActionList actions = new ActionList();
+        if (location.containsAnActor() && !location.getActor().hasStatus(RideStatus.class)) {
+            actions.add(new MountAction(this, location));
+        }
+        return actions;
+    }
+
+    /**
+     * While carried by a mounted rider, offers a dismount action.
+     *
+     * @param owner the actor carrying this rideable
+     * @param map the map the owner occupies
+     * @return an action list containing DismountAction when applicable
+     */
+    @Override
+    public ActionList allowableActions(Actor owner, GameMap map) {
+        return new ActionList(new DismountAction(this));
+    }
+}
