59a02ecb1f92af76d046af5cccd0941f7583aea3
Edmond <eche0116@student.monash.edu>
2026-06-05 15:10:36 +0800

    Refactor cuttable interface to take in location

--- src/game/items/Cuttable.java
+++ src/game/items/Cuttable.java
@@ -2,6 +2,7 @@ package game.items;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
 
 /**
  * Cuttable is an interface representing something that can be cut with a Plasma Cutter.
@@ -19,5 +20,5 @@ public interface Cuttable {
      * @param map   The map the actor is on.
      * @return A string description of what happened.
      */
-    String cutBy(Actor actor, GameMap map);
+    String cutBy(Actor actor, GameMap map, Location location);
 }
