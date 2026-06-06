3df2a4e54a52f8f9f8ff550bfe2e34de0a77dd0d
esoo0013 <esoo0013@student.monash.edu>
2026-06-06 18:47:41 +0800

    Uncommented my Test files for REQ 5

--- src/test/game/atmosphere/AirQualityReportTest.java
+++ src/test/game/atmosphere/AirQualityReportTest.java
@@ -1,36 +1,36 @@
-//package game.atmosphere;
-//
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-///**
-// * Tests the small AirQualityReport value object to make sure its constructor
-// * and getters keep the AQI and dominant pollutant values exactly as expected.
-// *
-// * @author esoo0013
-// */
-//
-//class AirQualityReportTest {
-//
-//    @Test
-//    void constructorStoresAqiValue() {
-//        AirQualityReport report = new AirQualityReport(4, "so2");
-//
-//        assertEquals(4, report.getAqi());
-//    }
-//
-//    @Test
-//    void constructorStoresDominantPollutant() {
-//        AirQualityReport report = new AirQualityReport(2, "no2");
-//
-//        assertEquals("no2", report.getDominantPollutant());
-//    }
-//
-//    @Test
-//    void gettersReturnValuesForDifferentReport() {
-//        AirQualityReport report = new AirQualityReport(5, "pm10");
-//
-//        assertEquals(5, report.getAqi());
-//        assertEquals("pm10", report.getDominantPollutant());
-//    }
-//}
+package game.atmosphere;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests the small AirQualityReport value object to make sure its constructor
+ * and getters keep the AQI and dominant pollutant values exactly as expected.
+ *
+ * @author esoo0013
+ */
+
+class AirQualityReportTest {
+
+    @Test
+    void constructorStoresAqiValue() {
+        AirQualityReport report = new AirQualityReport(4, "so2");
+
+        assertEquals(4, report.getAqi());
+    }
+
+    @Test
+    void constructorStoresDominantPollutant() {
+        AirQualityReport report = new AirQualityReport(2, "no2");
+
+        assertEquals("no2", report.getDominantPollutant());
+    }
+
+    @Test
+    void gettersReturnValuesForDifferentReport() {
+        AirQualityReport report = new AirQualityReport(5, "pm10");
+
+        assertEquals(5, report.getAqi());
+        assertEquals("pm10", report.getDominantPollutant());
+    }
+}
